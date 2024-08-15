cd7c72be354d6602736b99d6b2edc138eed9484d
salvadorgarrogomez <salvaxggz@gmail.com>
2024-08-15 23:06:46 +0200

    Toolbar Lemonade

--- Unidad 2/Ruta2_Clicks Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unidad 2/Ruta2_Clicks Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,25 +1,30 @@
 package com.example.lemonade
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,8 +34,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,25 +48,47 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LemonadeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    lemonadeProcess()
-                }
-            }
+            ViewContainer()
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun lemonadeProcess(modifier: Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize(Alignment.Center)){
-    var changeImage by remember {mutableStateOf(1)}
+fun ViewContainer() {
+    Scaffold (
+        topBar = { Toolbar() },
+        content = { Lemonade() }
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar() {
+    TopAppBar(
+        colors = topAppBarColors(
+            containerColor = colorResource(R.color.yellow)
+        ),
+        title = {
+            Row (
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 15.dp)
+            ){
+                Text(
+                    text = "Lemonade",
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun Lemonade(){
+    var changeImage by remember {mutableStateOf(1)}
     var clickCount by remember { mutableStateOf(0) }
     val requiredClicks = remember { (2..4).random() }
 
@@ -165,11 +195,12 @@ fun lemonadeProcess(modifier: Modifier = Modifier
     }
 }
 
+@ExperimentalMaterial3Api
 @Preview(showBackground = true,
     showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        lemonadeProcess()
+        ViewContainer()
     }
 }
